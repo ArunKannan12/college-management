85c5d1b25e91f6d9d344620287aae3d27b731920
arun <arunkannan962@gmail.com>
2023-04-30 13:17:21 +0530

    add students modified to mobile view

--- hospital/home/forms.py
+++ hospital/home/forms.py
@@ -3,6 +3,7 @@ from .models import *
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class stadm(ModelForm):
@@ -11,15 +12,24 @@ class stadm(ModelForm):
     date_of_birth=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Date of birth','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
     email=forms.EmailField(required=True,widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Enter the email ID','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
     phone=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter phone number','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
-    address=forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Enter your address','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
+    address=forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Enter your address','style':'width:300px;height:200px;border-radius:10px;border:2px skyblue solid;'}))
     city=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter your city','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
     state=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter your state','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
-    pin=forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Enter your pin','style':'width:300px;height:200px;border-radius:10px;border:2px skyblue solid;'}))
+    pin=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter your pin','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
     
     class Meta:
         model=admission
         fields=['firstname','lastname','date_of_birth','email','gender','phone','address','city','state','pin','courses_available','image']
-
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if admission.objects.filter(email=email).exists():
+            raise ValidationError("An user with this email already exists!")
+        return email
+    def clean_phone(self):
+        phone= self.cleaned_data["phone"]
+        if admission.objects.filter(phone=phone).exists():
+            raise ValidationError("An user with this phone number already exists!")
+        return phone
 class CustomUserForm(UserCreationForm):
     username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter user name'}))
     email=forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'enter E-mail'}))
@@ -28,6 +38,11 @@ class CustomUserForm(UserCreationForm):
     class Meta:
         model=User
         fields=['username','email','password1','password2']
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("An user with this email already exists!")
+        return email
 class Feedbackform(ModelForm):
     name=forms.CharField(required=True ,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter your name','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
     feedback=forms.CharField(required=True ,widget=forms.Textarea(attrs={'class':'form-control','placeholder':'type your feedback','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
--- hospital/home/models.py
+++ hospital/home/models.py
@@ -1,34 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+alpha=RegexValidator(r'^[a-zA-Z]{3,}','only alphabets are allowed and name should be more tha 3 charachets')
+numbers=RegexValidator('^(?:(?:\+|0{0,2})91(\s*[\ -]\s*)?|[0]?)?[789]\d{9}|(\d[ -]?){10}\d$','Enter a Valid Indian Phone Number')
+pincode=RegexValidator('^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$','Enter a valid pin code')
 # Create your models here.
 class departments(models.Model):
     depts=models.CharField(max_length=100)
     def __str__(self):
         return self.depts
 class staffdetail(models.Model):
-    staff_name=models.CharField(max_length=100)
+    staff_name=models.CharField(max_length=100,validators=[alpha],null=True,blank=True)
     depts=models.ForeignKey(departments,on_delete=models.CASCADE,null=True)
     staff_image=models.ImageField(upload_to='staff_images',null=True)
     def __str__(self):
         return self.staff_name.capitalize()
 class hods(models.Model):
-    hod_name=models.CharField(max_length=100)
+    hod_name=models.CharField(max_length=100,validators=[alpha],null=True,blank=True)
     depts=models.ForeignKey(departments,on_delete=models.CASCADE,null=True)
     hod_image=models.ImageField(upload_to='hod_images',null=True)
     def __str__(self):
         return self.hod_name.capitalize() + " ( " + str(self.depts) + " )"
 class admission(models.Model):
-    firstname=models.CharField(max_length=100)
-    lastname=models.CharField(max_length=50)
+    
+    firstname=models.CharField(max_length=100,validators=[alpha],null=True,blank=True)
+    lastname=models.CharField(max_length=50,validators=[alpha],null=True,blank=True)
     date_of_birth=models.DateField(auto_now=False, auto_now_add=False)
     email=models.EmailField(max_length=254)
     GENDER=(('MALE','MALE'),('FEMALE','FEMALE'))
     gender=models.CharField(max_length=50,choices=GENDER,null=True)
-    phone=models.CharField(max_length=10)
+    phone=models.CharField(max_length=10,validators=[numbers])
     address=models.TextField(max_length=200,null=True)
     city=models.CharField(max_length=50,null=True)
     state=models.CharField(max_length=50,null=True)
-    pin=models.CharField(max_length=6,null=True)
+    pin=models.CharField(max_length=6,null=True,validators=[pincode])
     COURSES=(('B.SC.GEOGRAPHY','B.SC.GEOGRAPHY'),('B.SC.MICROBIOLOGY','B.SC.MICROBIOLOGY'),('B.SC.COMPUTER SCIENCE','B.SC.COMPUTER SCIENCE'),('B.SC.PHYSICS','B.SC.PHYSICS'),('B.SC.MATHEMATICS','B.SC.MATHEMATICS'),('B.COM IT','B.COM IT'),('B.COM CA','B.COM CA'),('B.COM','B.COM'),('B.B.A BUSINESS ADMINISTRATION','B.B.A BUSINESS ADMINISTRATION'),('BBA.CA','BBA.CA'),('B.A ENGLISH','B.A ENGLISH'),('B.COM','B.COM'),('B.Sc.Cs','B.Sc.Cs'),('BCA','BCA'))
     courses_available=models.CharField(max_length=50,choices=COURSES,null=True)
     image=models.ImageField(upload_to='sample_images',null=True,blank=True)
@@ -46,7 +51,7 @@ class admission(models.Model):
             URL=''
         return URL
 class Feedback(models.Model):
-    name=models.CharField(max_length=50,null=False,blank=True)
+    name=models.CharField(max_length=50,validators=[alpha],null=True,blank=True)
     dept=models.ForeignKey(departments, on_delete=models.CASCADE,blank=False,null=True)
     feedback=models.TextField(max_length=500,null=True,blank=True)
     def __str__(self):
