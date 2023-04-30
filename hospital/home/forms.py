7f16a8c095f265f7cd007f7acd3ad578bf933a84
arun <arunkannan962@gmail.com>
2023-04-30 10:52:06 +0530

    about,add students,home,loginpage,signup modified

--- hospital/home/forms.py
+++ hospital/home/forms.py
@@ -6,16 +6,16 @@ from django import forms
 
 
 class stadm(ModelForm):
-    firstname=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Firstname'}))
-    lastname=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Lastname'}))
-    date_of_birth=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Date of birth'}))
-    email=forms.EmailField(required=True,widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Enter the email ID'}))
-    phone=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter phone number'}))
-    address=forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Enter your address'}))
-    city=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter your city'}))
-    state=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter your state'}))
-    pin=forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Enter your pin'}))
-
+    firstname=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Firstname','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
+    lastname=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Lastname','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
+    date_of_birth=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Date of birth','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
+    email=forms.EmailField(required=True,widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Enter the email ID','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
+    phone=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter phone number','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
+    address=forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Enter your address','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
+    city=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter your city','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
+    state=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter your state','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
+    pin=forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Enter your pin','style':'width:300px;height:200px;border-radius:10px;border:2px skyblue solid;'}))
+    
     class Meta:
         model=admission
         fields=['firstname','lastname','date_of_birth','email','gender','phone','address','city','state','pin','courses_available','image']
@@ -29,8 +29,8 @@ class CustomUserForm(UserCreationForm):
         model=User
         fields=['username','email','password1','password2']
 class Feedbackform(ModelForm):
-    name=forms.CharField(required=True ,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter your name'}))
-    feedback=forms.CharField(required=True ,widget=forms.Textarea(attrs={'class':'form-control','placeholder':'type your feedback'}))
+    name=forms.CharField(required=True ,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter your name','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
+    feedback=forms.CharField(required=True ,widget=forms.Textarea(attrs={'class':'form-control','placeholder':'type your feedback','style':'width:300px;height:50px;border-radius:10px;border:2px skyblue solid;'}))
     class Meta:
         model=Feedback
         fields=['name','dept','feedback']
